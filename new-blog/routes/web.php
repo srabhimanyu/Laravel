b229c8b07061290355366cb190f0cba9f9ee69b2
unknown <srabhimanyu@gmail.com>
2021-05-13 17:33:13 +0530

    Exploring the Routes in Laravel

--- new-blog/routes/web.php
+++ new-blog/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin/posts', function () {
+    
     return view('welcome');
+   // return "admin is here";
 });
