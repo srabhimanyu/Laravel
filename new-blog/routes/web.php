d15c9fe6dbf33a7205621971e468a205f577cfb0
srabhimanyu <srabhimanyu@gmail.com>
2021-07-01 12:11:26 +0530

    Routing in Laravel

--- new-blog/routes/web.php
+++ new-blog/routes/web.php
@@ -1,3 +1,4 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/example', function () {
     return view('welcome');
+});
+Route::get('/admin/posts', function () {
+    return "admin is here";
 });
